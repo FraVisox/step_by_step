fd1abc227b23dddf44b02cd60563160c67b4a84b
boorgoof <federico.31mene@icloud.com>
2024-05-18 00:09:09 +0200

    ui non ancora funzionante ma compila, ops prima piccolo errore

--- app/src/main/java/com/example/room/database/ui/DailyRecordsAdapter.kt
+++ app/src/main/java/com/example/room/database/ui/DailyRecordsAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.GridLayoutManager
@@ -37,7 +38,7 @@ class DailyRecordsAdapter : ListAdapter<UserActivityRecord, DailyRecordsAdapter.
     // Returns a new ViewHolder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DailyRecordsViewHolder {
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.daily_records_item, parent, false)
+            .inflate(R.layout.fragment_show_records, parent, false)
 
         return DailyRecordsViewHolder(view)
     }
@@ -58,24 +59,28 @@ class DailyRecordsAdapter : ListAdapter<UserActivityRecord, DailyRecordsAdapter.
     class DailyRecordsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         val dateOfRecords: TextView = itemView.findViewById(R.id.DateOfRecords)
-        val steps : TextView = itemView.findViewById(R.id.StepsCard)
-        val stepsImage : ImageView = itemView.findViewById(R.id.StepsCard)
 
-        val calories = itemView.findViewById<TextView>(R.id.CaloriesCard)
-        val caloriesImage = itemView.findViewById<ImageView>(R.id.CaloriesCard)
+        val steps : TextView = itemView.findViewById(R.id.countSteps)
+        val progressBarSteps: ProgressBar = itemView.findViewById(R.id.progressbarSteps)
+        //val stepsImage : ImageView = itemView.findViewById(R.id.imageSteps)
 
-        val distance = itemView.findViewById<TextView>(R.id.DistanceCard)
-        val distanceImage = itemView.findViewById<ImageView>(R.id.DistanceCard)
+        val calories = itemView.findViewById<TextView>(R.id.countCalories)
+        val progressBarCalories: ProgressBar = itemView.findViewById(R.id.progressbarCalories)
+        //val caloriesImage : ImageView = itemView.findViewById(R.id.imageCalories)
 
+        val distance = itemView.findViewById<TextView>(R.id.countDistance)
+        val progressBarDistance: ProgressBar = itemView.findViewById(R.id.progressbarDistance)
+        //val distanceImage : ImageView = itemView.findViewById(R.id.imageDistance)
         fun bind(date: String, countSteps: String, countCalories: String, countDistance: String ) {
+
             dateOfRecords.text = date
             steps.text = countSteps
             calories.text = countCalories
             distance.text = countDistance
 
-            stepsImage.setImageResource(R.drawable.baseline_directions_run_24)
-            caloriesImage.setImageResource(R.drawable.calories)
-            distanceImage.setImageResource(R.drawable.distance)
+            progressBarSteps.progress = countSteps.toInt()
+            progressBarCalories.progress = countCalories.toInt()
+            progressBarDistance.progress = countDistance.toInt()
 
         }
     }
