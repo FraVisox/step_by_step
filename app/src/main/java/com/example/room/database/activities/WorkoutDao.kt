4bd635ae7f8ba5c6c81948c2582f93ac6a193b64
Francesco Visona <visona.francesco@gmail.com>
2024-05-19 12:00:15 +0200

    Modifica minima al db

--- app/src/main/java/com/example/room/database/activities/WorkoutDao.kt
+++ app/src/main/java/com/example/room/database/activities/WorkoutDao.kt
@@ -7,52 +7,34 @@ import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface WorkoutDao {
+interface WorkoutDao { //TODO: replace o ignore?
 
-    // Questa query seleziona tutti i dati dalla tabella 'calories_table' e li ordina per data in ordine crescente
+    //WORKOUTS
     @Query("SELECT * FROM workout_table ORDER BY date ASC")
     fun getAllActivitiesOrderedByDate(): Flow<List<Workout>>
 
-    /*
-    @Query("SELECT * FROM workout_table WHERE date = date('now', 'localtime') ORDER BY date ASC")
-    fun getTodayActivities(): Flow<List<Workout>>
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(workout: Workout)
 
-    @Query("SELECT * FROM workout_table WHERE date >= date('now', 'weekday 0', '-6 days') ORDER BY date ASC")
-    fun getWeeklyActivities(): Flow<List<Workout>>
+    @Query("DELETE FROM workout_table")
+    suspend fun deleteAllActivities()
 
-    @Query("SELECT * FROM workout_table WHERE date >= date('now', 'start of month') ORDER BY date ASC")
-    fun getMonthlyActivities(): Flow<List<Workout>>
+    @Query("DELETE FROM workout_table WHERE workoutId = :id")
+    fun deleteWorkout(id: Int)
 
-     */
-
-    /*
-    @Query("SELECT * FROM workout_table WHERE activityId = :id")
-    fun getPointsOfActivity(id : Int): Flow<List<WorkoutTrackPoint>>
-
-     */
+    //POINTS OF WORKOUTS
+    @Query("SELECT * FROM point_table WHERE workoutId = :id")
+    fun getPointsOfWorkout(id : Int): Flow<List<WorkoutTrackPoint>>
 
     @Query("SELECT * FROM point_table")
     fun getAllPoints(): Flow<List<WorkoutTrackPoint>>
 
-    // Inserisce un nuovo record nella tabella. Se si verifica un conflitto (es. stesso uid), l'operazione di inserimento verrà ignorata.
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insert(activity: Workout)
-
-    /*
-    // Inserisce un nuovo record nella tabella. Se si verifica un conflitto (es. stesso uid), l'operazione di inserimento verrà ignorata.
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(track: WorkoutTrackPoint)
 
-     */
-
-    // Cancella tutti i dati dalla tabella
-    @Query("DELETE FROM workout_table")
-    suspend fun deleteAllActivities()
-
-    /*
-    // Cancella tutti i dati dalla tabella
     @Query("DELETE FROM point_table")
-    suspend fun deleteAllTracks()
+    suspend fun deleteAllPoints()
 
-     */
+    @Query("DELETE FROM point_table WHERE workoutId = :id")
+    fun deleteWorkoutPoints(id: Int)
 }
